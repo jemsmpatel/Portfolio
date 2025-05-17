17c098d1616efdf14d329b16dbc9bdc94d0aa54f
jems <jemsmpatel2005@gmail.com>
2025-05-17 22:18:04 +0530

    Update page.jsx
    updated flask app url

--- src/app/projects/page.jsx
+++ src/app/projects/page.jsx
@@ -76,7 +76,7 @@ export default function Projects() {
                             </div>
                         </Link>
                         <div className="p-4">
-                            <Link href={`https://e-commerce-site-v9ib.onrender.com/`} target="_blank">
+                            <Link href={`https://e-commerce-site-9xzp.onrender.com/`} target="_blank">
                                 <h3 className="font-semibold text-xl mb-2">E-commerce Platform</h3>
                                 <p className="text-sm text-muted-foreground mb-4">
                                     This is my first e-commerce project using Flask. The frontend is built with React, Vite, and Tailwind CSS. The backend is powered by Flask, and MongoDB is used for the database. Files are stored on Cloudinary. The project is responsive but still incomplete — many features will be added later.
